456b91eb8501a81ea2a9bbb8633cb4e5df514b7b
cyberkillua <moshood988@gmail.com>
2021-02-07 22:09:41 +0100

    landing page

--- pages/Landingpage.vue
+++ pages/Landingpage.vue
@@ -1,5 +1,23 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-card class="top">
+      <v-card-title
+        class="text-h4 grey--text text--lighten-1 font-weight-medium flex justify-center"
+      >
+        chatBOX
+      </v-card-title>
+      <v-card-subtitle
+        class="text-center text-black text-subtitle-1 font-weight-bold"
+      >
+        Welcome to chatBOX
+      </v-card-subtitle>
+      <v-card-text class="text-black font-weight-medium text-center">
+        Please remember to keep it respectful and to follow our user
+        guidelines.<br />
+        You can press the side menu to check to the available chatrooms
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -8,4 +26,14 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.top {
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 150px;
+}
+a {
+  text-decoration: none;
+}
+</style>
